b970b1c98595d5f1023d2ef32c86670fd2441461
Alexandre <Alexandre@DESKTOP-P4C2FN3>
2024-06-18 10:49:25 +0200

    Add null object for attribute

---
+++ src/com/pinguela/yourpc/model/NullAttribute.java
@@ -0,0 +1,56 @@
+package com.pinguela.yourpc.model;
+
+import java.util.Collection;
+
+public class NullAttribute extends Attribute<String> {
+	
+	public NullAttribute() {
+		super.setName("");
+	}
+
+	@Override
+	public void setName(String name) {}
+
+	@Override
+	public String getValueAt(int index) {
+		return null;
+	}
+
+	@Override
+	public boolean addValue(Long id, Object value) {
+		return false;
+	}
+
+	@Override
+	public void addAll(Collection<AttributeValue<String>> newValues) {}
+
+	@Override
+	public void removeValue(int index) {}
+
+	@Override
+	public void removeValue(AttributeValue<?> value) {}
+
+	@Override
+	public void removeAllValues() {}
+
+	@Override
+	public Attribute<String> clone() {
+		return this;
+	}
+
+	@Override
+	public Attribute<String> trim() {
+		return this;
+	}
+
+	@Override
+	public String getDataTypeIdentifier() {
+		return VARCHAR;
+	}
+
+	@Override
+	public int getValueHandlingMode() {
+		return SET;
+	}
+
+}
