05639517845f97d4f0db9452789ae8d25f2d82aa
Alexandre Perebaskine <pereb27@gmail.com>
2024-09-16 16:18:30 +0200

    Add Hibernate dependency

---
+++ src/com/pinguela/yourpc/util/HibernateUtils.java
@@ -0,0 +1,43 @@
+package com.pinguela.yourpc.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataBuilder;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.reflections.Reflections;
+
+import com.pinguela.yourpc.model.AbstractValueObject;
+
+import jakarta.persistence.Entity;
+
+public class HibernateUtils {
+	
+	private static final String MODEL_PACKAGE_NAME = "com.pinguela.yourpc.model";
+	
+	public static final SessionFactory SESSION_FACTORY;
+	
+	static {
+		ServiceRegistry standardRegistry =
+		        new StandardServiceRegistryBuilder().build();
+
+		MetadataSources sources = new MetadataSources(standardRegistry);
+		
+		Reflections reflections = new Reflections(MODEL_PACKAGE_NAME);
+		for (Class<?> clazz : reflections.getSubTypesOf(AbstractValueObject.class)) {
+			if (clazz.isAnnotationPresent(Entity.class)) {
+				sources.addAnnotatedClass(clazz);
+			}
+		}
+		
+		MetadataBuilder builder = sources.getMetadataBuilder();
+		builder.applyPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+		
+		Metadata metadata = builder.build();
+		
+		SESSION_FACTORY = metadata.buildSessionFactory();
+	}
+
+}
