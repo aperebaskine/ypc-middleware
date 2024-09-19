fb07192d999eefcf259998691a785c319adcfd67
aperebaskine <aperebaskine@DIP-34.dip.local>
2024-09-19 21:20:29 +0200

    Update initialization implementation

--- src/com/pinguela/yourpc/model/NullObject.java
+++ src/com/pinguela/yourpc/model/NullObject.java
@@ -1,113 +1,166 @@
 package com.pinguela.yourpc.model;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.InvocationHandler;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 
 public class NullObject {
-	
-	private static NullObjectInvocationHandler handler = new NullObjectInvocationHandler();
-	
+
+	private static Set<String> objectMethods;
+
+	static {
+		objectMethods = new HashSet<String>();
+		for (Method method : Object.class.getMethods()) {
+			objectMethods.add(method.getName());
+		}
+	}
+
+	private static NullObjectMethodInterceptor interceptor = new NullObjectMethodInterceptor();
+
 	private static Map<Class<?>, Object> defaultReturnValues;
 	private static Map<Method, Object> customReturnValues;
 	private static Map<Class<?>, Object> instances;
-	
+
 	static {
 		defaultReturnValues = new HashMap<>();
 		customReturnValues = new HashMap<>();
 		instances = new HashMap<>();
-		
+
+		defaultReturnValues.put(void.class, null);
 		defaultReturnValues.put(boolean.class, false);
-        defaultReturnValues.put(byte.class, (byte) 0);
-        defaultReturnValues.put(char.class, '\u0000');
-        defaultReturnValues.put(short.class, (short) 0);
-        defaultReturnValues.put(int.class, 0);
-        defaultReturnValues.put(long.class, 0L);
-        defaultReturnValues.put(float.class, 0.0f);
-        defaultReturnValues.put(double.class, 0.0d);
-        defaultReturnValues.put(String.class, "");
-        defaultReturnValues.put(Collection.class, Collections.EMPTY_LIST);
-        defaultReturnValues.put(Map.class, Collections.EMPTY_MAP);
-        defaultReturnValues.put(Set.class, Collections.EMPTY_SET);
+		defaultReturnValues.put(byte.class, (byte) 0);
+		defaultReturnValues.put(char.class, '\u0000');
+		defaultReturnValues.put(short.class, (short) 0);
+		defaultReturnValues.put(int.class, 0);
+		defaultReturnValues.put(long.class, 0L);
+		defaultReturnValues.put(float.class, 0.0f);
+		defaultReturnValues.put(double.class, 0.0);
+
+		defaultReturnValues.put(String.class, "");
+		defaultReturnValues.put(Object.class, null);
+		defaultReturnValues.put(List.class, Collections.emptyList());
+		defaultReturnValues.put(Set.class, Collections.emptySet());
+		defaultReturnValues.put(Map.class, Collections.emptyMap());
 	}
-	
+
 	private NullObject() {
 	}
-	
+
 	public static <T> T getInstance(Class<T> clazz) {
 		return getInstance(clazz, null, null);
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public static <T> T getInstance(Class<T> clazz, Method[] customReturnValueMethods, Object[] customReturnValues) {
-		
+	public static <T> T getInstance(Class<T> target, Method[] customReturnValueMethods, Object[] customReturnValues) {
+
+		if (Modifier.isFinal(target.getModifiers())) {
+			throw new IllegalArgumentException("Cannot create null object for a final class.");
+		}
+
 		T instance = null;
 
-		if (instances.containsKey(clazz)) {
-			instance = (T) instances.get(clazz);
-		} else {
-			if (clazz.isInterface()) {
-				instance = (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[] {clazz}, handler);
-			} else {
-				Enhancer enhancer = new Enhancer();
-				enhancer.setSuperclass(clazz);
-				enhancer.setCallback(handler);
-				instance = (T) enhancer.create();
+		try {
+			if (instances.containsKey(target)) {
+				return (T) instances.get(target);
 			}
-			instances.put(clazz, instance);
+
+			if (target.isInterface()) {
+				return (T) Proxy.newProxyInstance(target.getClassLoader(), new Class<?>[] {target}, interceptor);
+			}
+
+			try {
+				target.getDeclaredConstructor();
+			} catch (NoSuchMethodException e) {
+				throw new IllegalArgumentException(String.format(
+						"Cannot create null object: class %s doesn't provide a no-arg constructor.", target.getName()));
+			}
+
+			Enhancer enhancer = new Enhancer();
+			enhancer.setSuperclass(target);
+			enhancer.setCallback(interceptor);
+			instance = (T) enhancer.create();
+
+		} finally {
+			if (instance != null) {
+				setCustomReturnValues(customReturnValueMethods, customReturnValues);
+				instances.put(target, instance);
+			}
 		}
-		
-		setCustomReturnValues(customReturnValueMethods, customReturnValues);
+
 		return instance;
 	}
-	
+
 	public static void setCustomReturnValues(Method[] methods, Object[] returnValues) {
-		
+
 		if (methods == null || returnValues == null) {
 			return;
 		}
-		
+
 		if (methods.length != returnValues.length) {
 			throw new IllegalArgumentException("Class and value arrays must be of equal size.");
 		}
-		
+
 		Map<Method, Object> returnValueMap = new HashMap<>();
-		
+
 		for (int i = 0; i < methods.length; i++) {
-			
+
 			Method method = methods[i];
 			Object returnValue = returnValues[i];
-			
-			if (!method.getReturnType().isAssignableFrom(returnValue.getClass())) {
+
+			if (!method.getReturnType().isInstance(returnValue)) {
 				throw new IllegalArgumentException(
-						String.format("Incorrect return type %s for method %s", 
-								returnValue.getClass().getName(), method.getName()));
+						String.format("Invalid return type %s for method %s, expecting return type %s.", 
+								returnValue.getClass().getName(), method.getName(), method.getReturnType().getName()));
 			} 
-			
+
 			returnValueMap.put(method, returnValue);
 		}
-		
+
 		customReturnValues.putAll(returnValueMap);
 	}
-	
-	private static class NullObjectInvocationHandler 
-	implements java.lang.reflect.InvocationHandler, InvocationHandler {
+
+	private static class NullObjectMethodInterceptor 
+	implements MethodInterceptor, java.lang.reflect.InvocationHandler {
 
 		@Override
 		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			
-			return customReturnValues.containsKey(method) ?
-					customReturnValues.get(method) :
-						defaultReturnValues.get(method.getReturnType());
+			return intercept(proxy, method, args, null);
 		}
-		
+
+		@Override
+		public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+
+			if (objectMethods.contains(method.getName())) {
+				return proxy != null ? proxy.invokeSuper(obj, args) : method.invoke(obj, args);
+			}
+
+			if (customReturnValues.containsKey(method)) {
+				return customReturnValues.get(method);
+			}
+
+			Class<?> returnType = method.getReturnType();
+
+			if (defaultReturnValues.containsKey(returnType)) {
+				return defaultReturnValues.get(returnType);
+			}
+
+			try {
+				return getInstance(returnType);
+			} catch (Exception e) {
+				return null;
+			}
+		}
+
 	}
 
 }
