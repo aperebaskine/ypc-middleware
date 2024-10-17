0748ee59704c6825f695dce0c72a6525e73c2b74
Alexandre <pereb27@gmail.com>
2024-10-17 18:56:18 +0200

    Update test dependencies

--- test/com/pinguela/yourpc/service/AddressServiceTest.java
+++ test/com/pinguela/yourpc/service/AddressServiceTest.java
@@ -1,9 +1,9 @@
 package com.pinguela.yourpc.service;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
