6897adb55ab50ef47f22131f744fcd8c08cbc34d
Alexandre Perebaskine <pereb27@gmail.com>
2024-11-08 14:19:13 +0100

    Add base class for Criteria to SQL transformer

---
+++ src/com/pinguela/yourpc/dao/impl/builder/AbstractCriteriaToSQLTransformer.java
@@ -0,0 +1,10 @@
+package com.pinguela.yourpc.dao.impl.builder;
+
+import com.pinguela.yourpc.model.AbstractEntity;
+import com.pinguela.yourpc.model.AbstractEntityCriteria;
+
+public abstract class AbstractCriteriaToSQLTransformer<E extends AbstractEntity<?>, C extends AbstractEntityCriteria<?, E>> {
+	
+	
+
+}
