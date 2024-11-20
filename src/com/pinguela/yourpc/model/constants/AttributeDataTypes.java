3d5ed050dcb2b20924c5ba300aba6abe8f3cbc3f
Alexandre <pereb27@gmail.com>
2024-11-20 21:36:22 +0100

    Add validation method

--- src/com/pinguela/yourpc/model/constants/AttributeDataTypes.java
+++ src/com/pinguela/yourpc/model/constants/AttributeDataTypes.java
@@ -1,5 +1,9 @@
 package com.pinguela.yourpc.model.constants;
 
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Defines the constants corresponding to the SQL database's primary key
  * that are used to identify the attribute's data type.
@@ -9,12 +13,30 @@ package com.pinguela.yourpc.model.constants;
  */
 public final class AttributeDataTypes {
 	
-	private AttributeDataTypes() {
-	}
-
 	public static final String VARCHAR = "VAR";
 	public static final String BIGINT = "INT";
 	public static final String DECIMAL = "DEC";
 	public static final String BOOLEAN = "BOO";
+	
+	private static Set<String> types = initSet();
+	
+	private AttributeDataTypes() {
+	}
+	
+	private static Set<String> initSet() {
+		Set<String> set = new HashSet<String>();
+		try {
+			for (Field field : AttributeDataTypes.class.getFields()) {
+				set.add((String) field.get(null));
+			}
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+		return set;
+	}
+	
+	public static boolean isValidType(String type) {
+		return types.contains(type);
+	}
 
 }
