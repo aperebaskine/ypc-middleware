cc56219eefb7dc4349d1c5eeef4fd5420ed6373e
Alexandre Perebaskine <pereb27@gmail.com>
2024-11-25 12:58:05 +0100

    Remove redundant import

--- src/com/pinguela/yourpc/model/ProductCriteria.java
+++ src/com/pinguela/yourpc/model/ProductCriteria.java
@@ -3,7 +3,6 @@ package com.pinguela.yourpc.model;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import com.pinguela.yourpc.model.dto.AbstractProductDTO;
 import com.pinguela.yourpc.model.dto.AttributeDTO;
