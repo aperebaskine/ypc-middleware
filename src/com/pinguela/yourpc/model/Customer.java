02c0203b1c6497e871d85b3f99376f0e20f6cba7
Alexandre Perebaskine <pereb27@gmail.com>
2024-12-08 13:45:14 +0100

    Make Customer cloneable

--- src/com/pinguela/yourpc/model/Customer.java
+++ src/com/pinguela/yourpc/model/Customer.java
@@ -1,10 +1,12 @@
 package com.pinguela.yourpc.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class Customer
-extends AbstractPerson {
+extends AbstractPerson
+implements Cloneable {
 	
 	private List<Address> addresses = null;
 	
@@ -18,6 +20,18 @@ extends AbstractPerson {
 
 	public void setAddresses(List<Address> addressList) {
 		this.addresses = addressList;
+	}
+	
+	@Override
+	public Customer clone() {
+		try {
+			Customer clone = (Customer) super.clone();
+			clone.setCreationDate(new Date(this.getCreationDate().getTime()));
+			clone.setAddresses(new ArrayList<Address>(addresses));
+			return clone;
+		} catch (CloneNotSupportedException e) {
+			throw new AssertionError(e);
+		}
 	}
 
 }
